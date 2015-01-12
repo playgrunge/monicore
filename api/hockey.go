67585179e28209009c9234cd58f4c45441d327c0
vagrant <bodyjar@hotmail.com>
2015-01-12 01:58:25 +0000

    mongodb persistence for hockey api

--- api/hockey.go
+++ api/hockey.go
@@ -4,6 +4,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"encoding/json"
+	"github.com/Igor-K/mgo"
+	"reflect"
 )
 
 type HockeyApi struct{}
@@ -13,6 +16,7 @@ func (h *HockeyApi) SendApi(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(res)
 }
@@ -30,5 +34,45 @@ func (h *HockeyApi) GetApi() ([]byte, error) {
 		return nil, err
 	}
 
+	h.updateData(robots)
 	return robots, nil
+}
+
+func (h *HockeyApi) GetData() (map[string]interface{}) {
+	session, err := mgo.Dial("localhost")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer session.Close()
+
+    c := session.DB("monicore").C("hockey")
+    var r map[string]interface{}
+    err = c.Find(nil).Sort("$natural:-1").Limit(1).One(&r);
+    delete(r, "_id")
+    return r
+}
+
+func (h *HockeyApi) updateData(data []byte) {
+	session, err := mgo.Dial("localhost")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer session.Close()
+
+    c := session.DB("monicore").C("hockey")
+    var r map[string]interface{}
+    err = c.Find(nil).Sort("$natural:-1").Limit(1).One(&r);
+    delete(r, "_id")
+
+	log.Println("Data compared...")
+	var d map[string]interface{}
+	json.Unmarshal(data, &d)
+	eq := reflect.DeepEqual(r, d)
+	if eq {
+		log.Println("Data identical")
+	} else {
+		err = c.Insert(d)
+		log.Println("Data updated")
+	}
+	
 }
