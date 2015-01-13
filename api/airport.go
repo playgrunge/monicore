6567757fb5bf33394bfcd7b6258954a81f4cae70
bodyjar <bodyjar@hotmail.com>
2015-01-13 04:39:09 +0000

    new apis for airport delays and weather

--- api/airport.go
+++ api/airport.go
@@ -9,7 +9,21 @@ import (
 type AirportApi struct{}
 
 func (a *AirportApi) SendApi(w http.ResponseWriter, r *http.Request) {
-	res, err := a.GetApi()
+	typ := r.FormValue("t")
+	if len(typ) == 0 {
+		typ = "default"
+	}
+	var res []byte
+	var err error
+	switch typ {
+	case "delay":
+		res, err = a.GetDelay()
+	case "weather":
+		res, err = a.GetWeather()
+	default:
+		res, err = a.GetApi()
+	}
+
 	if err != nil {
 		return
 	}
@@ -32,6 +46,35 @@ func (a *AirportApi) GetApi() ([]byte, error) {
 	return robots, err
 }
 
-func (a *AirportApi) updateData(data []byte) {
+func (a *AirportApi) GetWeather() ([]byte, error) {
+	res, err := http.Get("http://api.flightstats.com/flex/weather/rest/v1/json/all/YUL?appId=" + GetConfig().Flightstats.AppId + "&appKey=" + GetConfig().Flightstats.AppKey + "&utc=false&numHours=1&maxFlights=5")
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	robots, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	return robots, err
+}
 
+func (a *AirportApi) GetDelay() ([]byte, error) {
+	res, err := http.Get("http://api.flightstats.com/flex/delayindex/rest/v1/json/airports/YUL?appId=" + GetConfig().Flightstats.AppId + "&appKey=" + GetConfig().Flightstats.AppKey + "&utc=false&numHours=1&maxFlights=5")
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	robots, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	return robots, err
+}
+
+func (a *AirportApi) updateData(data []byte) {
 }
